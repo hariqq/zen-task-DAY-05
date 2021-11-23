f4ccfc462d6f3959d0df3c025774dc729adabf85
hariqq <94283564+hariqq@users.noreply.github.com>
2021-11-23 16:53:00 +0530

    Update and rename 1.g)remove_duplicate_array.js to 1.g)remove duplicate array anonymous function.js

---
+++ 1.g)remove duplicate array anonymous function.js
@@ -0,0 +1,10 @@
+//7.Remove duplicates from an array using anonymous function:
+let arrayWithDup = [22, 22, 11, 10, 11, 56, 10, 1, 2, 6, 7, 56, 3, 9]
+
+let resultArrayWoDup = ((arrayWithDup, index) => {
+
+    return [...new Set(arrayWithDup)]
+
+})(arrayWithDup)
+
+console.log("Array without Duplicates " + resultArrayWoDup)
