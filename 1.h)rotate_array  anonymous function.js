db4036934d8f21898d5b8f5640235aff27ecdafa
hariqq <94283564+hariqq@users.noreply.github.com>
2021-11-23 16:54:22 +0530

    Update and rename 1.h)rotate_array.js to 1.h)rotate_array  anonymous function.js

---
+++ 1.h)rotate_array  anonymous function.js
@@ -0,0 +1,19 @@
+//8.Rotate an array by k times and return the rotated array using anonymous function:
+
+let arrforrotation = [0, 1, 5, 6, 7, 9]
+let resultArrayShifted = ((array, k) => {
+
+    for (let i = 0; i < k; i++) {
+        let temp = array.shift()
+        array.push(temp)
+    }
+    return array
+
+})(arrforrotation, 3)
+
+console.log("Array roatated k times: " + resultArrayShifted)
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
