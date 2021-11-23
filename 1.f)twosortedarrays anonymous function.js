c44ba51b3d96b47898de3f36c001c35e423d7316
hariqq <94283564+hariqq@users.noreply.github.com>
2021-11-23 16:51:06 +0530

    Update 1.f)twosortedarrays anonymous function.js

--- 1.f)twosortedarrays anonymous function.js
+++ 1.f)twosortedarrays anonymous function.js
@@ -31,28 +31,3 @@ let resultMedian = function (arr1, arr2) {
     return
 }(arr1, arr2)
 
-//7.Remove duplicates from an array
-let arrayWithDup = [22, 22, 11, 10, 11, 56, 10, 1, 2, 6, 7, 56, 3, 9]
-
-let resultArrayWoDup = ((arrayWithDup, index) => {
-
-    return [...new Set(arrayWithDup)]
-
-})(arrayWithDup)
-
-console.log("Array without Duplicates " + resultArrayWoDup)
-
-//8.Rotate an array by k times and return the rotated array
-
-let arrforrotation = [0, 1, 5, 6, 7, 9]
-let resultArrayShifted = ((array, k) => {
-
-    for (let i = 0; i < k; i++) {
-        let temp = array.shift()
-        array.push(temp)
-    }
-    return array
-
-})(arrforrotation, 3)
-
-console.log("Array roatated k times: " + resultArrayShifted)
